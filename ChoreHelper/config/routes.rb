971dc8511adc1d8c691e773519240428352ac1ba
Thao Tran <Tranth916@gmail.com>
2016-09-06 10:29:01 -0700

    rerouted localhost:3000 to go directly to the occupants

--- ChoreHelper/config/routes.rb
+++ ChoreHelper/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'occupants/index'
   
-  root 'welcome#index'
+  root 'occupants#index'
 
   resources :occupants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
